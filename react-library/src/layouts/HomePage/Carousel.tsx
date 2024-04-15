1c0cf690787fbcce81ed80b47fe3b4e2b7e9fb5f
carrynong2 <carrynong2@gmail.com>
2024-04-15 11:39:44 +0700

    init frontend

---
+++ react-library/src/layouts/HomePage/Carousel.tsx
@@ -0,0 +1,12 @@
+export const Carousel = () => {
+  return (
+    <div className="container mt-5" style={{ height: 550 }}>
+      <div className="homepage-carousel-title">
+        <h3>Find your next "I stayed up too late reading" book.</h3>
+      </div>
+      <div id="carouselExampleControls" className="carousel carousel-dark">
+
+      </div>
+    </div>
+  )
+}
