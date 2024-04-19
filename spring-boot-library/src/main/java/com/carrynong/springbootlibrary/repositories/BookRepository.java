a28fe53532279e938b71f64ff22d5e4a94ab08cb
carrynong2 <carrynong2@gmail.com>
2024-04-19 09:26:09 +0700

    add pagination and search

--- spring-boot-library/src/main/java/com/carrynong/springbootlibrary/repositories/BookRepository.java
+++ spring-boot-library/src/main/java/com/carrynong/springbootlibrary/repositories/BookRepository.java
@@ -1,7 +1,12 @@
 package com.carrynong.springbootlibrary.repositories;
 
 import com.carrynong.springbootlibrary.entities.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
